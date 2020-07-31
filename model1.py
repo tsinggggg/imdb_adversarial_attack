dc354fa6ff5a3eb54b48a34a27daf00a388fa55e
qc <c29qian@uwaterloo.ca>
2020-07-31 02:26:44 -0400

    v0.2.2, basic sklearn usage

--- model1.py
+++ model1.py
@@ -1,24 +1,30 @@
 import pandas as pd
+import sys
+sys.path.append("/home/qc/work/imdb_adversarial_attack")
+
+# from imdb_adversarial_attack.vectorizor import spacy_tokenizer
+
+# def spacy_tokenizer(sentence):
+#     import spacy
+#     nlp = spacy.load('en_core_web_md')
+#     mytokens = nlp(sentence)
+#     mytokens = [word.lemma_ for word in mytokens if not word.is_punct and not word.is_stop]
+#     return mytokens
+#
+#
+# spacy_tokenizer.__module__ = "vectorizor"
 
 
 class Tokenizer:
     def __init__(self):
         import pandas as pd
-
-        def spacy_tokenizer(sentence):
-            import spacy
-            nlp = spacy.load('en_core_web_md')
-            mytokens = nlp(sentence)
-            mytokens = [word.lemma_ for word in mytokens if not word.is_punct and not word.is_stop]
-            return mytokens
-
-        self.t = pd.read_pickle("./tf_idf_vectorizor.pkl")
+        self.t = pd.read_pickle("~/work/imdb_adversarial_attack/tf_idf_vectorizor2.pkl")
 
     def encode(self, x):
         return self.t.transform(x)
 
 
-model = pd.read_pickle('./tf_idf_logistic_reg.pkl')
+model = pd.read_pickle('~/work/imdb_adversarial_attack/tf_idf_logistic_reg.pkl')
 
 tokenizer = Tokenizer()
 
--- test_data.py
+++ test_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_csv('test_data.csv')
+df = pd.read_csv('test_data.csv', header=None)
 df.columns = ['label', 'text']
 dataset = df[['text', 'label']].values.tolist()
     
